540636290d5c38aad0549e660faf388a17abb90c
hariganet <hariganet.sandbox@gmail.com>
2014-03-04 21:09:18 +0900

    add fs

--- chap-1/server.js
+++ chap-1/server.js
@@ -1,5 +1,38 @@
 var http = require('http');
+var path = require('path');
+var fs = require('fs');
+
+var mimeTypes = {
+  '.js': 'text/javascript',
+  '.html': 'text/html',
+  '.css': 'text/css'
+};
+
 http.createServer(function(request, response){
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('Woohoo!');
+  if(request.url === '/favicon.ico'){
+    response.writeHead(404);   
+    response.end();
+    return;
+  }
+
+  var lookup = path.basename(decodeURI(request.url)) || 'index.html',
+  f = 'content/' + lookup;
+  fs.exists(f, function(exists){
+    if(exists){
+      fs.readFile(f, function(err, data){
+        if(err){
+          response.writeHead(500);
+          response.end('Server Error!');
+          return;
+        }
+        var headers = {'Content-Type': mimeTypes[path.extname(f)] };
+        response.writeHead(200, headers);
+        response.end(data);
+
+      });
+      return;
+    }
+    response.writeHead(404);
+    response.end('ページが見つかりません。');
+  });
 }).listen(8080);
